lazyoop/immwrt-mt798x-Stitch-Enhanced

commit 3fc0f36464c1ef60918c7e16b8662924b5e73ded

procd: add default timeout for reload trigger actions
Signed-off-by: Felix Fietkau <nbd@nbd.name>

package/system/procd/files/procd.sh
@@ -32,6 +32,7 @@
 
 . $IPKG_INSTROOT/usr/share/libubox/jshn.sh
 
+PROCD_RELOAD_DELAY=1000
 _PROCD_SERVICE=
 
 _procd_call() {
@@ -221,6 +222,11 @@ _procd_set_param() {
 	esac
 }
 
+_procd_add_timeout() {
+	[ "$PROCD_RELOAD_DELAY" -gt 0 ] && json_add_int "" "$PROCD_RELOAD_DELAY"
+	return 0
+}
+
 _procd_add_interface_trigger() {
 	json_add_array
 	_procd_add_array_data "$1"
@@ -240,6 +246,8 @@ _procd_add_interface_trigger() {
 
 	json_close_array
 	json_close_array
+
+	_procd_add_timeout
 }
 
 _procd_add_reload_interface_trigger() {
@@ -271,6 +279,8 @@ _procd_add_config_trigger() {
 	json_close_array
 
 	json_close_array
+
+	_procd_add_timeout
 }
 
 _procd_add_raw_trigger() {

commit c0eb9120a80dc9cae6e3c7b68a86a5cbf6e27ea1

procd: fix default timeout for reload trigger actions
Default trigger action timeout was added to procd.sh in commit 3fc0f3646
(procd: add default timeout for reload trigger actions)
However, the timeout value was not placed under the correct JSON-script
array nesting level and thus did not apply.

To fix this and make the timeout actually apply to the reload triggers,
we place it in the correct scope, that is the per-trigger array.

Fixes: 3fc0f36464c1ef60918c7e16b8662924b5e73ded
Signed-off-by: Denis Osvald <denis.osvald@sartura.hr>

package/system/procd/files/procd.sh
@@ -251,9 +251,8 @@ _procd_add_interface_trigger() {
 	json_close_array
 
 	json_close_array
-	json_close_array
-
 	_procd_add_timeout
+	json_close_array
 }
 
 _procd_add_reload_interface_trigger() {
@@ -283,10 +282,8 @@ _procd_add_config_trigger() {
 	json_close_array
 
 	json_close_array
-
-	json_close_array
-
 	_procd_add_timeout
+	json_close_array
 }
 
 _procd_add_raw_trigger() {