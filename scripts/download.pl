lazyoop/immwrt-mt798x-Stitch-Enhanced

commit 0018a7c43012bd08f9d03fd278e1c7d0eae8c66f

scripts/download.pl: add SHA-256 support
Signed-off-by: Felix Fietkau <nbd@openwrt.org>

SVN-Revision: 48253

scripts/download.pl
@@ -11,12 +11,12 @@ use warnings;
 use File::Basename;
 use File::Copy;
 
-@ARGV > 2 or die "Syntax: $0 <target dir> <filename> <md5sum> <url filename> [<mirror> ...]\n";
+@ARGV > 2 or die "Syntax: $0 <target dir> <filename> <hash> <url filename> [<mirror> ...]\n";
 
 my $url_filename;
 my $target = shift @ARGV;
 my $filename = shift @ARGV;
-my $md5sum = shift @ARGV;
+my $file_hash = shift @ARGV;
 $url_filename = shift @ARGV unless $ARGV[0] =~ /:\/\//;
 my $scriptdir = dirname($0);
 my @mirrors;
@@ -59,8 +59,20 @@ sub which($) {
 	return $res;
 }
 
-my $md5cmd = which("md5sum") || which("md5") || die 'no md5 checksum program found, please install md5 or md5sum';
-chomp $md5cmd;
+sub hash_cmd() {
+	my $len = length($file_hash);
+	my $cmd;
+
+	$len == 64 and return "openssl dgst -sha256";
+	$len == 32 and do {
+		my $cmd = which("md5sum") || which("md5") || die 'no md5 checksum program found, please install md5 or md5sum';
+		chomp $cmd;
+		return $cmd;
+	};
+	return undef;
+}
+
+my $hash_cmd = hash_cmd();
 
 sub download
 {
@@ -105,20 +117,24 @@ sub download
 		print("Copying $filename from $link\n");
 		copy($link, "$target/$filename.dl");
 
-		if (system("$md5cmd '$target/$filename.dl' > '$target/$filename.md5sum'")) {
-			print("Failed to generate md5 sum for $filename\n");
-			return;
-		}
+		$hash_cmd and do {
+			if (system("$hash_cmd '$target/$filename.dl' > '$target/$filename.hash'")) {
+				print("Failed to generate hash for $filename\n");
+				return;
+			}
+		};
 	} else {
 		open WGET, "wget -t5 --timeout=20 --no-check-certificate $options -O- '$mirror/$url_filename' |" or die "Cannot launch wget.\n";
-		open MD5SUM, "| $md5cmd > '$target/$filename.md5sum'" or die "Cannot launch md5sum.\n";
+		$hash_cmd and do {
+			open MD5SUM, "| $hash_cmd > '$target/$filename.hash'" or die "Cannot launch $hash_cmd.\n";
+		};
 		open OUTPUT, "> $target/$filename.dl" or die "Cannot create file $target/$filename.dl: $!\n";
 		my $buffer;
 		while (read WGET, $buffer, 1048576) {
-			print MD5SUM $buffer;
+			$hash_cmd and print MD5SUM $buffer;
 			print OUTPUT $buffer;
 		}
-		close MD5SUM;
+		$hash_cmd and close MD5SUM;
 		close WGET;
 		close OUTPUT;
 
@@ -129,15 +145,17 @@ sub download
 		}
 	}
 
-	my $sum = `cat "$target/$filename.md5sum"`;
-	$sum =~ /^(\w+)\s*/ or die "Could not generate md5sum\n";
-	$sum = $1;
+	$hash_cmd and do {
+		my $sum = `cat "$target/$filename.hash"`;
+		$sum =~ /^(\w+)\s*/ or die "Could not generate file hash\n";
+		$sum = $1;
 
-	if (($md5sum =~ /\w{32}/) and ($sum ne $md5sum)) {
-		print STDERR "MD5 sum of the downloaded file does not match (file: $sum, requested: $md5sum) - deleting download.\n";
-		cleanup();
-		return;
-	}
+		if ($sum ne $file_hash) {
+			print STDERR "MD5 sum of the downloaded file does not match (file: $sum, requested: $file_hash) - deleting download.\n";
+			cleanup();
+			return;
+		}
+	};
 
 	unlink "$target/$filename";
 	system("mv", "$target/$filename.dl", "$target/$filename");
@@ -147,7 +165,7 @@ sub download
 sub cleanup
 {
 	unlink "$target/$filename.dl";
-	unlink "$target/$filename.md5sum";
+	unlink "$target/$filename.hash";
 }
 
 @mirrors = localmirrors();

commit 55c6884b43501326ea6ec672c9dc992a47804e7d

scripts/download.pl: fail loudly if provided hash is unsupported
Currently, if the provided hash is unsupported (length different from 32
or 64 bytes), we happily download the requested file without any kind of
checksum verification.

This is quite dangerous and may provide a false sense of security, because
a single typo in the hash (e.g. one character deleted by mistake) may skip
checksum verification entirely.

Instead, fail immediately if we don't support the provided hash.
In particular, if an external package repository decides to change the
hash algorithm one day, we will now fail loudly instead of skipping
checksum verification without complaints.

Note: if some users of scripts/download.pl knowingly provide an empty hash
because they don't need checksum verification, this change will break
them.  This does not seem to be the case currently, but if this feature is
ever needed, an option should be added to download.pl instead of relying
on the hash being empty.

Fixes: 0018a7c43012 ("scripts/download.pl: add SHA-256 support")

Signed-off-by: Baptiste Jonglez <git@bitsofnetworks.org>

scripts/download.pl
@@ -88,6 +88,7 @@ sub download_cmd($) {
 }
 
 my $hash_cmd = hash_cmd();
+$hash_cmd or die "Cannot find appropriate hash command, ensure the provided hash is either a MD5 or SHA256 checksum.\n";
 
 sub download
 {