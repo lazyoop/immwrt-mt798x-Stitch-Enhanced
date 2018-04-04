lazyoop/immwrt-mt798x-Stitch-Enhanced

commit 3c96977b9a2a09db7c42279ee277e1f0828acf05

base-files: add more name source to get_dt_led helper function
Not all LED driver are using the label devicetree property for the led
name. Add support for the TI/National Semiconductor LP55xx Led Drivers,
which are using the chan-name property for the led name, as fallback.

Signed-off-by: Mathias Kresin <dev@kresin.me>

package/base-files/files/lib/functions/leds.sh
@@ -8,7 +8,10 @@ get_dt_led() {
 	local nodepath="$basepath/aliases/led-$1"
 
 	[ -f "$nodepath" ] && ledpath=$(cat "$nodepath")
-	[ -n "$ledpath" ] && label=$(cat "$basepath$ledpath/label")
+	[ -n "$ledpath" ] && \
+		label=$(cat "$basepath$ledpath/label" 2>/dev/null) || \
+		label=$(cat "$basepath$ledpath/chan-name" 2>/dev/null) || \
+		echo "led label not found" 1>&2
 
 	echo "$label"
 }

commit f3119895e80b54c28b9a197d88eb3936bb5afecc

base-files: get_dt_led: don't warn about missing led
It's intentional that some boards within a target don't have all LEDs
which are tried to be setup in a common script. Don't show a warning in
such cases.

Fixes: 3c96977b9a2a ("base-files: add more name source to get_dt_led helper function")
Signed-off-by: Mathias Kresin <dev@kresin.me>

package/base-files/files/lib/functions/leds.sh
@@ -10,8 +10,7 @@ get_dt_led() {
 	[ -f "$nodepath" ] && ledpath=$(cat "$nodepath")
 	[ -n "$ledpath" ] && \
 		label=$(cat "$basepath$ledpath/label" 2>/dev/null) || \
-		label=$(cat "$basepath$ledpath/chan-name" 2>/dev/null) || \
-		echo "led label not found" 1>&2
+		label=$(cat "$basepath$ledpath/chan-name" 2>/dev/null)
 
 	echo "$label"
 }