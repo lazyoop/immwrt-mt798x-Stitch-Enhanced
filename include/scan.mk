lazyoop/immwrt-mt798x-Stitch-Enhanced

commit 48bbb7eb2a120d2ab1fc02e242c9ac4d59ac0b28

build: Honour NO_COLOR in include/scan.mk
The sender domain has a DMARC Reject/Quarantine policy which disallows
sending mailing list messages using the original "From" header.

To mitigate this problem, the original message has been wrapped
automatically by the mailing list software.
Hi all:

This is my first OpenWrt patch. I am a clean, pure newbie! 8-)

Honour NO_COLOR in Makefile function 'progress' in include/scan.mk, in the same way that include/verbose.mk does.

Signed-off-by: R. Diez <rdiezmail-openwrt@yahoo.com>

include/scan.mk
@@ -19,9 +19,15 @@ else
 endif
 
 ifeq ($(IS_TTY),1)
-  define progress
+  ifneq ($(strip $(NO_COLOR)),1)
+    define progress
 	printf "\033[M\r$(1)" >&2;
-  endef
+    endef
+  else
+    define progress
+	:;
+    endef
+  endif
 else
   define progress
 	:;

commit 5ab7a63ebf82a72dce05521ac4c0286813b444e3

build: Fix print without color
The original patch removed the printing completely, just remove the
color.

Fixes: 48bbb7eb2a12  ("build: Honour NO_COLOR in include/scan.mk")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

include/scan.mk
@@ -25,7 +25,7 @@ ifeq ($(IS_TTY),1)
     endef
   else
     define progress
-	:;
+	printf "\r$(1)" >&2;
     endef
   endif
 else