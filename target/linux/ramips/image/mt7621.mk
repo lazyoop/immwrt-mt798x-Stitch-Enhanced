lazyoop/immwrt-mt798x-Stitch-Enhanced

commit 5079c6256ab0dca86194cc71cd3ca1e716622f6c

ramips: remove RAM size from device name for UniElec devices
UniElec devices are the last ones in ramips target still having
the RAM size in device name although RAM size is auto-detected.

Remove this from device name, compatible, etc., as it's not
required and might be misleading to users and developers adding
device support copying those devices.

Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/ramips/image/mt7621.mk
@@ -682,24 +682,25 @@ define Device/ubiquiti_edgerouterx-sfp
 endef
 TARGET_DEVICES += ubiquiti_edgerouterx-sfp
 
-define Device/unielec_u7621-06-256m-16m
+define Device/unielec_u7621-06-16m
   MTK_SOC := mt7621
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := UniElec
   DEVICE_MODEL := U7621-06
-  DEVICE_VARIANT := 256M RAM/16M flash
+  DEVICE_VARIANT := 16M
   DEVICE_PACKAGES := kmod-ata-core kmod-ata-ahci kmod-sdhci-mt7620 kmod-usb3
-  SUPPORTED_DEVICES += u7621-06-256M-16M
+  SUPPORTED_DEVICES += u7621-06-256M-16M unielec,u7621-06-256m-16m
 endef
 TARGET_DEVICES += unielec_u7621-06-256m-16m
 
-define Device/unielec_u7621-06-512m-64m
+define Device/unielec_u7621-06-64m
   MTK_SOC := mt7621
   IMAGE_SIZE := 65216k
   DEVICE_VENDOR := UniElec
   DEVICE_MODEL := U7621-06
-  DEVICE_VARIANT := 512M RAM/64M flash
+  DEVICE_VARIANT := 64M
   DEVICE_PACKAGES := kmod-ata-core kmod-ata-ahci kmod-sdhci-mt7620 kmod-usb3
+  SUPPORTED_DEVICES += unielec,u7621-06-512m-64m
 endef
 TARGET_DEVICES += unielec_u7621-06-512m-64m
 

target/linux/ramips/image/mt76x8.mk
@@ -454,14 +454,14 @@ define Device/tplink_tl-wr902ac-v3
 endef
 TARGET_DEVICES += tplink_tl-wr902ac-v3
 
-define Device/unielec_u7628-01-128m-16m
+define Device/unielec_u7628-01-16m
   MTK_SOC := mt7628an
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := UniElec
   DEVICE_MODEL := U7628-01
-  DEVICE_VARIANT := 128M RAM/16M flash
+  DEVICE_VARIANT := 16M
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport
-  SUPPORTED_DEVICES += u7628-01-128M-16M
+  SUPPORTED_DEVICES += u7628-01-128M-16M unielec,u7628-01-128m-16m
 endef
 TARGET_DEVICES += unielec_u7628-01-128m-16m
 

target/linux/ramips/base-files/etc/board.d/01_leds
@@ -401,7 +401,7 @@ tplink,tl-wr902ac-v3)
 	ucidef_set_led_wlan "wlan2g" "wlan2g" "$boardname:green:wlan" "phy0tpt"
 	ucidef_set_led_switch "lan" "lan" "$boardname:green:lan" "switch0" "0x10"
 	;;
-unielec,u7628-01-128m-16m)
+unielec,u7628-01-16m)
 	ucidef_set_led_switch "lan1" "lan1" "u7628-01:green:lan1" "switch0" "0x2"
 	ucidef_set_led_switch "lan2" "lan2" "u7628-01:green:lan2" "switch0" "0x4"
 	ucidef_set_led_switch "lan3" "lan3" "u7628-01:green:lan3" "switch0" "0x8"

target/linux/ramips/base-files/etc/board.d/02_network
@@ -82,8 +82,8 @@ ramips_setup_interfaces()
 	telco-electronics,x1|\
 	totolink,a7000r|\
 	totolink,lr1200|\
-	unielec,u7621-06-256m-16m|\
-	unielec,u7621-06-512m-64m|\
+	unielec,u7621-06-16m|\
+	unielec,u7621-06-64m|\
 	wavlink,wl-wn570ha1|\
 	wavlink,wl-wn575a3|\
 	xiaomi,miwifi-mini|\
@@ -211,7 +211,7 @@ ramips_setup_interfaces()
 	tplink,tl-wr841n-v13|\
 	tplink,tl-wr841n-v14|\
 	tplink,tl-wr842n-v5|\
-	unielec,u7628-01-128m-16m|\
+	unielec,u7628-01-16m|\
 	ubiquiti,edgerouterx|\
 	ubiquiti,edgerouterx-sfp|\
 	upvel,ur-326n4g|\

target/linux/ramips/dts/mt7621_unielec_u7621-06-16m.dts
@@ -40,8 +40,8 @@
 #include <dt-bindings/input/input.h>
 
 / {
-	compatible = "unielec,u7621-06-256m-16m", "unielec,u7621-06", "mediatek,mt7621-soc";
-	model = "UniElec U7621-06 (256M RAM/16M flash)";
+	compatible = "unielec,u7621-06-16m", "unielec,u7621-06", "mediatek,mt7621-soc";
+	model = "UniElec U7621-06 (16M flash)";
 };
 
 &spi0 {

target/linux/ramips/dts/mt7621_unielec_u7621-06-64m.dts
@@ -41,8 +41,8 @@
 #include <dt-bindings/input/input.h>
 
 / {
-	compatible = "unielec,u7621-06-512m-64m", "unielec,u7621-06", "mediatek,mt7621-soc";
-	model = "UniElec U7621-06 (512M RAM/64M flash)";
+	compatible = "unielec,u7621-06-64m", "unielec,u7621-06", "mediatek,mt7621-soc";
+	model = "UniElec U7621-06 (64M flash)";
 };
 
 &spi0 {

target/linux/ramips/dts/mt7628an_unielec_u7628-01-16m.dts
@@ -37,8 +37,8 @@
 #include "mt7628an_unielec_u7628-01.dtsi"
 
 / {
-	compatible = "unielec,u7628-01-128m-16m", "unielec,u7628-01", "mediatek,mt7628an-soc";
-	model = "UniElec U7628-01 (128M RAM/16M flash)";
+	compatible = "unielec,u7628-01-16m", "unielec,u7628-01", "mediatek,mt7628an-soc";
+	model = "UniElec U7628-01 (16M flash)";
 };
 
 &spi0 {

commit e0adada5fc71646a4d5569d47821145c913b2998

ramips: fix TARGET_DEVICES after UniElec rename
Device node names were updated, but updating TARGET_DEVICES
was overlooked.

Fixes: 5079c6256ab0 ("ramips: remove RAM size from device name
for UniElec devices")

Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/ramips/image/mt7621.mk
@@ -691,7 +691,7 @@ define Device/unielec_u7621-06-16m
   DEVICE_PACKAGES := kmod-ata-core kmod-ata-ahci kmod-sdhci-mt7620 kmod-usb3
   SUPPORTED_DEVICES += u7621-06-256M-16M unielec,u7621-06-256m-16m
 endef
-TARGET_DEVICES += unielec_u7621-06-256m-16m
+TARGET_DEVICES += unielec_u7621-06-16m
 
 define Device/unielec_u7621-06-64m
   MTK_SOC := mt7621
@@ -702,7 +702,7 @@ define Device/unielec_u7621-06-64m
   DEVICE_PACKAGES := kmod-ata-core kmod-ata-ahci kmod-sdhci-mt7620 kmod-usb3
   SUPPORTED_DEVICES += unielec,u7621-06-512m-64m
 endef
-TARGET_DEVICES += unielec_u7621-06-512m-64m
+TARGET_DEVICES += unielec_u7621-06-64m
 
 define Device/wevo_11acnas
   MTK_SOC := mt7621

target/linux/ramips/image/mt76x8.mk
@@ -463,7 +463,7 @@ define Device/unielec_u7628-01-16m
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport
   SUPPORTED_DEVICES += u7628-01-128M-16M unielec,u7628-01-128m-16m
 endef
-TARGET_DEVICES += unielec_u7628-01-128m-16m
+TARGET_DEVICES += unielec_u7628-01-16m
 
 define Device/vocore_vocore2
   MTK_SOC := mt7628an