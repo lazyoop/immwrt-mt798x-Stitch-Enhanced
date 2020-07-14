lazyoop/immwrt-mt798x-Stitch-Enhanced

commit f4220f09cbf726fe9067116ef24c2349cb3de09c

vxlan: add options to enable and disable UDP checksums
Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>

package/network/config/vxlan/files/vxlan.sh
@@ -15,8 +15,8 @@ vxlan_generic_setup() {
 
 	local link="$cfg"
 
-	local port vid ttl tos mtu macaddr zone
-	json_get_vars port vid ttl tos mtu macaddr zone
+	local port vid ttl tos mtu macaddr zone rxcsum txcsum
+	json_get_vars port vid ttl tos mtu macaddr zone rxcsum txcsum
 
 
 	proto_init_update "$link" 1
@@ -36,6 +36,8 @@ vxlan_generic_setup() {
 	[ -n "$port" ] && json_add_int port "$port"
 	[ -n "$vid" ] && json_add_int id "$vid"
 	[ -n "$macaddr" ] && json_add_string macaddr "$macaddr"
+	[ -n "$rxcsum" ] && json_add_boolean rxcsum "$rxcsum"
+	[ -n "$txcsum" ] && json_add_boolean txcsum "$txcsum"
 	json_close_object
 
 	proto_close_tunnel

package/network/config/vxlan/Makefile
@@ -1,7 +1,7 @@
 include $(TOPDIR)/rules.mk
 
 PKG_NAME:=vxlan
-PKG_VERSION:=1
+PKG_VERSION:=2
 PKG_LICENSE:=GPL-2.0
 
 include $(INCLUDE_DIR)/package.mk

commit 9f0fefbb3c24fecf85464177a33a5031a865b3b5

vxlan: fix udp checksum control
So far, passing "rxcsum" and "txcsum" had no effect.

Fixes: f4220f09cbf7 ("vxlan: add options to enable and disable UDP
checksums")

Signed-off-by: Johannes Kimmel <fff@bareminimum.eu>
[add Fixes:]
Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

package/network/config/vxlan/files/vxlan.sh
@@ -129,6 +129,8 @@ vxlan_generic_init_config() {
 	proto_config_add_int "ttl"
 	proto_config_add_int "tos"
 	proto_config_add_int "mtu"
+	proto_config_add_boolean "rxcsum"
+	proto_config_add_boolean "txcsum"
 	proto_config_add_string "macaddr"
 }
 