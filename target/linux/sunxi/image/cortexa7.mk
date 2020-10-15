lazyoop/immwrt-mt798x-Stitch-Enhanced

commit 224de2d253e999c8739fdfab893741dd481781bb

sunxi: BPi-M2U kernel modules for onboard WiFi
Signed-off-by: Hal Martin <hal.martin@gmail.com>

target/linux/sunxi/image/cortexa7.mk
@@ -74,7 +74,8 @@ TARGET_DEVICES += lemaker_bananapi
 define Device/lemaker_bananapi-m2-ultra
   DEVICE_VENDOR := LeMaker
   DEVICE_MODEL := Banana Pi M2 Ultra
-  DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-ata-sunxi
+  DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-ata-sunxi kmod-brcmfmac \
+	brcmfmac-firmware-43430a0-sdio wpad-basic
   SOC := sun8i-r40
 endef
 TARGET_DEVICES += lemaker_bananapi-m2-ultra

commit 3f7c59f2a0fcf2f6ddc586a1f82075c47b7500e9

sunxi: use wpad-basic-wolfssl for Sinovoip Banana Pi M2 Ultra
The idea of commit 2d456fcd7f72 was to use wpad-basic-wolfssl
consistently throughout the whole trunk, so use it here as well.

Fixes: 224de2d253e9 ("BPi-M2U kernel modules for onboard WiFi")

Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/sunxi/image/cortexa7.mk
@@ -75,7 +75,7 @@ define Device/sinovoip_bananapi-m2-ultra
   DEVICE_VENDOR := Sinovoip
   DEVICE_MODEL := Banana Pi M2 Ultra
   DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-ata-sunxi kmod-brcmfmac \
-	brcmfmac-firmware-43430a0-sdio wpad-basic
+	brcmfmac-firmware-43430a0-sdio wpad-basic-wolfssl
   SUPPORTED_DEVICES:=lemaker,bananapi-m2-ultra
   SOC := sun8i-r40
 endef