lazyoop/immwrt-mt798x-Stitch-Enhanced

commit 9f10c301f8ead9cf5d83311d07cfd6ecc08b2a31

autocore: ethinfo: adapt LuCI style
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

package/lean/autocore/files/x86/rpcd_21_ethinfo.js
@@ -19,12 +19,12 @@ return L.Class.extend({
 	render: function(data) {
 		var ethinfo = Array.isArray(data[0].ethinfo) ? data[0].ethinfo : [];
 
-		var table = E('div', { 'class': 'table' }, [
-			E('div', { 'class': 'tr table-titles' }, [
-				E('div', { 'class': 'th' }, _('Ethernet Name')),
-				E('div', { 'class': 'th' }, _('Link Status')),
-				E('div', { 'class': 'th' }, _('Speed')),
-				E('div', { 'class': 'th' }, _('Duplex'))
+		var table = E('td', { 'class': 'table' }, [
+			E('tr', { 'class': 'tr table-titles' }, [
+				E('td', { 'class': 'th' }, _('Ethernet Name')),
+				E('td', { 'class': 'th' }, _('Link Status')),
+				E('td', { 'class': 'th' }, _('Speed')),
+				E('td', { 'class': 'th' }, _('Duplex'))
 			])
 		]);
 

commit 8fc4925f18efc61509a29923e913e87b18e37303

autocore: ethinfo: adapt LuCI style again
Fixes: 9f10c301f8 ("autocore: ethinfo: adapt LuCI style")

Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

package/lean/autocore/files/x86/rpcd_21_ethinfo.js
@@ -19,12 +19,12 @@ return L.Class.extend({
 	render: function(data) {
 		var ethinfo = Array.isArray(data[0].ethinfo) ? data[0].ethinfo : [];
 
-		var table = E('td', { 'class': 'table' }, [
+		var table = E('table', { 'class': 'table' }, [
 			E('tr', { 'class': 'tr table-titles' }, [
-				E('td', { 'class': 'th' }, _('Ethernet Name')),
-				E('td', { 'class': 'th' }, _('Link Status')),
-				E('td', { 'class': 'th' }, _('Speed')),
-				E('td', { 'class': 'th' }, _('Duplex'))
+				E('th', { 'class': 'th' }, _('Ethernet Name')),
+				E('th', { 'class': 'th' }, _('Link Status')),
+				E('th', { 'class': 'th' }, _('Speed')),
+				E('th', { 'class': 'th' }, _('Duplex'))
 			])
 		]);
 