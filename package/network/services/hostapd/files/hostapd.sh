lazyoop/immwrt-mt798x-Stitch-Enhanced

commit 1741f5cfb327bc848d1058c53787e0c4deeb2acf

hostapd: add channel utilization as config option
Add the channel utilization as hostapd configuration option.

Signed-off-by: Nick Hainke <vincent@systemli.org>

package/network/services/hostapd/files/hostapd.sh
@@ -229,7 +229,8 @@ hostapd_set_bss_options() {
 		wps_independent wps_device_type wps_device_name wps_manufacturer wps_pin \
 		macfilter ssid wmm uapsd hidden short_preamble rsn_preauth \
 		iapp_interface eapol_version dynamic_vlan ieee80211w nasid \
-		acct_server acct_secret acct_port acct_interval
+		acct_server acct_secret acct_port acct_interval \
+		bss_load_update_period chan_util_avg_period
 
 	set_default isolate 0
 	set_default maxassoc 0
@@ -243,7 +244,9 @@ hostapd_set_bss_options() {
 	set_default tdls_prohibit 0
 	set_default eapol_version 0
 	set_default acct_port 1813
-
+	set_default bss_load_update_period 60
+	set_default chan_util_avg_period 600
+	
 	append bss_conf "ctrl_interface=/var/run/hostapd"
 	if [ "$isolate" -gt 0 ]; then
 		append bss_conf "ap_isolate=$isolate" "$N"
@@ -255,6 +258,8 @@ hostapd_set_bss_options() {
 		append bss_conf "ap_max_inactivity=$max_inactivity" "$N"
 	fi
 
+	append bss_conf "bss_load_update_period=$bss_load_update_period" "$N"
+	append bss_conf "chan_util_avg_period=$chan_util_avg_period" "$N"
 	append bss_conf "disassoc_low_ack=$disassoc_low_ack" "$N"
 	append bss_conf "preamble=$short_preamble" "$N"
 	append bss_conf "wmm_enabled=$wmm" "$N"

commit 23a4939d44dafd68d03f81b145940f7f9236153c

hostapd: fix handling of the channel utilization options
Commit 1741f5c ("hostapd: add channel utilization as config option") added the
two new uci options bss_load_update_period and chan_util_avg_period. However,
the corresponding "config_add_int" calls for these options weren't added, so
attempting to actually use these options and change their values is bound to
fail - they always stay at their defaults. Add the missing code to actually
make these options work.

Fixes: 1741f5c ("hostapd: add channel utilization as config option")
Signed-off-by: Timo Sigurdsson <public_timo.s@silentcreek.de>
(cherry picked from commit e6de30162e9383f419cd13cd265eb0b7e8c2337d)

package/network/services/hostapd/files/hostapd.sh
@@ -251,6 +251,8 @@ hostapd_common_add_bss_config() {
 	config_add_int acct_port
 	config_add_int acct_interval
 
+	config_add_int bss_load_update_period chan_util_avg_period
+
 	config_add_string dae_client
 	config_add_string dae_secret
 	config_add_int dae_port