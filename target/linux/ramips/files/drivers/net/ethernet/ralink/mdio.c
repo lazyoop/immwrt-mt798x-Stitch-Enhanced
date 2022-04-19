lazyoop/immwrt-mt798x-Stitch-Enhanced

commit 737793415cf2da138b73bafe57a904794f7ac0fb

ramips: mt7620n: add mdio node and disable port4 by default
Revert the changes I applied to beb1d3e985f8 ("ramips: mt7620n: enable
port 4 as EPHY by default").

The driver expects a node mdio-bus to be present, regardless of the
actual node status. If the node is missing the driver fails to load  with

  mtk_soc_eth 10100000.ethernet: no mdio-bus child node found

Disable port4 by default again. If the port is enabled but not present, a
"invalid port id 4" warning is shown during boot.

Fixes: FS#1428

Signed-off-by: Mathias Kresin <dev@kresin.me>

target/linux/ramips/dts/mt7620n.dtsi
@@ -293,11 +293,18 @@
 
 		mediatek,switch = <&gsw>;
 
+		mdio-bus {
+			#address-cells = <1>;
+			#size-cells = <0>;
+
+			status = "disabled";
+		};
+
 		port@4 {
 			compatible = "mediatek,mt7620a-gsw-port", "mediatek,eth-port";
 			reg = <4>;
 
-			status = "okay";
+			status = "disabled";
 		};
 	};
 

commit ee077c31623127f19d15e42f9042c63d0f40d9c6

ramips: mt7620: remove useless GMAC nodes
These nodes are used for configuring a GMAC interface
and for defining external PHYs to be accessed with MDIO.

None of this is possible on MT7620N, only MT7620A,
so remove them from all MT7620N DTS.

When the mdio-bus node is missing, the driver returns -NODEV
which causes the internal switch to not initialize.
Replace that return so that everything works without the DTS node.

Also, an extra kernel message to indicate for all error conditions
that mdio-bus is disabled.

Fixes: 737793415cf2 ("ramips: mt7620n: add mdio node and disable port4 by default")
Fixes: beb1d3e985f8 ("ramips: mt7620n: enable port 4 as EPHY by default")
Signed-off-by: Michael Pratt <mcpratt@pm.me>
(cherry picked from commit 7651c98b4121fed5080f6dc2f98b1a7313468efc)

target/linux/ramips/dts/mt7620n.dtsi
@@ -322,20 +322,6 @@
 		reset-names = "fe", "esw";
 
 		mediatek,switch = <&gsw>;
-
-		mdio-bus {
-			#address-cells = <1>;
-			#size-cells = <0>;
-
-			status = "disabled";
-		};
-
-		port@4 {
-			compatible = "mediatek,mt7620a-gsw-port", "mediatek,eth-port";
-			reg = <4>;
-
-			status = "disabled";
-		};
 	};
 
 	gsw: gsw@10110000 {

target/linux/ramips/dts/mt7620n_dlink_dwr-921-c1.dts
@@ -133,12 +133,6 @@
 	status = "okay";
 };
 
-&ethernet {
-	port@4 {
-		status = "okay";
-	};
-};
-
 &state_default {
 	default {
 		groups = "spi refclk", "i2c", "ephy", "wled";

target/linux/ramips/dts/mt7620n_dlink_dwr-922-e2.dts
@@ -136,12 +136,6 @@
 	status = "okay";
 };
 
-&ethernet {
-	port@4 {
-		status = "okay";
-	};
-};
-
 &state_default {
 	default {
 		groups = "spi refclk", "i2c", "ephy", "wled";

target/linux/ramips/files/drivers/net/ethernet/ralink/mdio.c
@@ -223,7 +223,8 @@ int fe_mdio_init(struct fe_priv *priv)
 	mii_np = of_get_child_by_name(priv->dev->of_node, "mdio-bus");
 	if (!mii_np) {
 		dev_err(priv->dev, "no %s child node found", "mdio-bus");
-		return -ENODEV;
+		err = 0;
+		goto err_no_bus;
 	}
 
 	if (!of_device_is_available(mii_np)) {
@@ -255,6 +256,8 @@ err_free_bus:
 	kfree(priv->mii_bus);
 err_put_node:
 	of_node_put(mii_np);
+err_no_bus:
+	dev_err(priv->dev, "%s disabled", "mdio-bus");
 	priv->mii_bus = NULL;
 	return err;
 }